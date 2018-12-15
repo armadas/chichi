8c4e86320e862e8731fd04fffee2e693fab3322b
Shisei Naka <nathaniel.naka@gmail.com>
2018-12-15 16:34:20 +0900

    該当ユーザーしか、roomにアクセスできない様に処理

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,8 @@ class Room < ApplicationRecord
   belongs_to :participant, class_name: 'User', foreign_key: 'participant_id'
     
   has_many :messages #一つの部屋に複数のメッセージが持てる
+
+  def is_authorized?(user_id)
+    return owner_id == user_id || participant_id == user_id
+  end
 end
