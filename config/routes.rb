af1898aeb6e027782bb8fb6a8d510100aff180af
Shisei Naka <nathaniel.naka@gmail.com>
2018-12-15 23:57:17 +0900

    room#showでチャットが表示、投稿できる様に改善

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :rooms
+  post "rooms/:id/create_message" => "rooms#create_message"
+
   devise_for :users
   get '/' => "home#top"
   get '/about' => "home#about"
