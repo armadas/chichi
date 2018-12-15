096cdc58fcfb0a29a2b9107d7fbcffa75c6114ee
Shisei Naka <nathaniel.naka@gmail.com>
2018-12-15 21:41:18 +0900

    Room tableにユーザーと紐づけるindexを貼り直す

--- db/migrate/20181215062721_create_rooms.rb
+++ db/migrate/20181215062721_create_rooms.rb
@@ -1,8 +1,8 @@
 class CreateRooms < ActiveRecord::Migration[5.2]
   def change
     create_table :rooms do |t|
-      t.integer :owner_id
-      t.integer :participant_id
+      t.integer :owner_id, index: true
+      t.integer :participant_id, index: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_15_064036) do
+ActiveRecord::Schema.define(version: 2018_12_15_075002) do
 
   create_table "messages", force: :cascade do |t|
-    t.integer "room_id_id"
-    t.integer "user_id_id"
+    t.integer "room_id"
+    t.integer "user_id"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["room_id_id"], name: "index_messages_on_room_id_id"
-    t.index ["user_id_id"], name: "index_messages_on_user_id_id"
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2018_12_15_064036) do
     t.integer "participant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_rooms_on_owner_id"
+    t.index ["participant_id"], name: "index_rooms_on_participant_id"
   end
 
   create_table "users", force: :cascade do |t|
